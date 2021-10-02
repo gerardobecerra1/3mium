87b3c19163db4748f931c525cbb1a06b6b6f035d
Luis Gerardo Becerra Jiménez <gerajmz@outlook.com>
2021-10-02 09:34:44 -0500

    Factorial

---
+++ Factorial/Factorial/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduzca un número entero: ");
+            int num = Convert.ToInt32(System.Console.ReadLine());
+            Console.WriteLine("Su factorial es: {0}", Factorial(num));
+        }
+        public static long Factorial(int n)
+        {
+            if (n == 1)
+                return 1;
+            return n * Factorial(n - 1);
+        }
+    }
+}
